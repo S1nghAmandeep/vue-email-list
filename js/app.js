21186aa47b6b32c0329f68ae0ad58c19f407ac0f
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-02 15:33:31 +0100

    generated-10-random-emails

--- js/app.js
+++ js/app.js
@@ -3,7 +3,28 @@ const { createApp, ref } = Vue
 createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        emailList: [],
       }
+      
+    },
+
+    methods: {
+    getEmail() {
+        for (let index = 0; index < 10; index++) {
+            axios.get(' https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((res) => {
+                this.emailList.push(res.data.response)
+            })  
+        }
+    },
+
+    },
+
+    mounted(){
+        this.getEmail()
+        console.log(this.getEmail)
     }
+
   }).mount('#app')
+
